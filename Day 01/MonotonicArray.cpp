a74f9e9099dea61e45cc5759e4aa342b3dd034dd
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-30 12:59:17 +0530

    Update MonotonicArray.cpp

--- Day 01/MonotonicArray.cpp
+++ Day 01/MonotonicArray.cpp
@@ -1,3 +1,33 @@
 
 
                                                                 // Monotonic Array  //
+
+
+
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int increase=0,decrease=0;
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(nums[i]<nums[i+1])
+            increase++;
+            else if(nums[i]>nums[i+1])
+            decrease++;
+            else
+            {
+                increase++;
+                decrease++;
+            }
+        }
+        cout<<decrease<<endl;
+        cout<<increase<<endl;
+        if(increase==nums.size()-1 || decrease==nums.size()-1)
+        return true;
+        else
+        return false;
+        
+        
+    }
+};
